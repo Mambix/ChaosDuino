98c7180b72ce6c61296dcf7a1ff3c6e4271796af
MambiX Ltd <ledi.mambix@gmail.com>
2018-05-22 00:48:41 +0100

    Initial commit

---
+++ chaosKey/chaosKey.ino
@@ -0,0 +1,20 @@
+#include <CRC32.h>
+
+const int PinEN = 5;
+const int InputPin = 15;
+
+uint8_t tmp;
+int val;
+
+void setup() {
+  pinMode(PinEN, OUTPUT);
+  Serial.begin(250000);
+
+  digitalWrite(PinEN, HIGH);
+}
+
+void loop() {
+  val = analogRead(0);
+  tmp = val & 0xff;
+  Serial.write(tmp);
+}
